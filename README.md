1da03a68df03244c589c9110307ecc7d458b70b8
Junaid Malik <Junaid.Malik@n2o.co.uk>
2017-10-04 16:26:57 +0100

    added custom filtering

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# APM
+# Angular Product Management
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.4.
 
---
+++ src/app/products/products.ts
@@ -0,0 +1,11 @@
+import { ProductListComponent } from './product-list.component';
+export interface IProduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  releaseDate: string;
+  description: string;
+  price: number;
+  starRating: number;
+  imageUrl: string;
+}
