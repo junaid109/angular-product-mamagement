25611fdf4546cd8e00ab3fb328bcc117f8989e72
Junaid <junmalik109@gmail.com>
2017-10-19 21:25:26 +0100

    added data retrieved from local json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string{
@@ -25,12 +26,10 @@ export class ProductListComponent implements OnInit {
 
     filteredProducts: IProduct[];
 
-    products: IProduct[] = [
-
-    ];
+    products: IProduct[] = [];
 
     constructor(private _productService: ProductService) {
-      this.filteredProducts = this.products;
+      // this.filteredProducts = this.products;
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -44,8 +43,12 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      this.products = this._productService.getProducts();
-      this.filteredProducts = this.products;
+      this._productService.getProducts()
+      .subscribe(products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+      },
+          error => this.errorMessage = <any>error);
     }
 
 }
